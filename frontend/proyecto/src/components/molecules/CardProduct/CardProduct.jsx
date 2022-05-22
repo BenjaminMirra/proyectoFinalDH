5917eab1eefccfec5bc9a62d51667eb3c834f978
afidal <analaurafidalgo@gmail.com>
2022-05-22 19:09:43 -0300

    Avances en el disenio interno de la card de producto

--- frontend/proyecto/src/components/molecules/CardProduct/CardProduct.jsx
+++ frontend/proyecto/src/components/molecules/CardProduct/CardProduct.jsx
@@ -1,27 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./CardProduct.css";
 import { Heading } from "../../atoms/Heading/Heading";
 import { Paragraph } from "../../atoms/paragraph/Paragraph";
 import { Button } from "../../atoms/Button/Button";
+import { Icon } from "../../atoms/Icon/Icon";
 
-
-export const CardProduct = ({url, titulo, descripcion, location, category}) => {
-    const [readMore,setReadMore]=useState(false);
-    return(
-        <div className="card-product">
-      <img src={url} alt={titulo} />
+export const CardProduct = ({
+  url,
+  titulo,
+  descripcion,
+  location,
+  category,
+}) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <div className="card-product">
+      <div className="card-product-img">
+        <img src={url} alt={titulo} />
+        <div className="fav">
+        <Icon icon="favorite" width="md" onClick={()=>{}}></Icon>
+        </div>
+      </div>
       <div className="card-product-text">
-      <Heading type="xs" title="h4" variant="primary">
-          {category}
+        <div className="cat-cat">
+        <Heading type="xs" title="h4" variant="terciary">
+          {category.toUpperCase()}
         </Heading>
+        <div className="product-cat-cat">
+        <Icon icon="star" width="xs" onClick={()=>{}}></Icon>
+        <Icon icon="star" width="xs" onClick={()=>{}}></Icon>
+        <Icon icon="star" width="xs" onClick={()=>{}}></Icon>
+        <Icon icon="star" width="xs" onClick={()=>{}}></Icon>
+        <Icon icon="star" width="xs" onClick={()=>{}}></Icon>
+        </div>
+        </div>
         <Heading type="md" title="h3" variant="primary">
           {titulo}
         </Heading>
         <Paragraph size="md" variant="secondary">
-          {descripcion} {titulo}
+          {descripcion}
         </Paragraph>
         <Button size="sm" label="ver detalle" variant="primary"></Button>
       </div>
-        </div>
-    );
-}
+    </div>
+  );
+};
