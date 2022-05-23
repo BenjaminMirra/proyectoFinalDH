fd7cbd870b7e04f52cfcef8ea18d9fb7ab44e335
afidal <analaurafidalgo@gmail.com>
2022-05-23 12:23:25 -0300

    Avances en la card de producto, modificacion de Heading.jsx para incluir h4

--- frontend/proyecto/src/App.js
+++ frontend/proyecto/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Products } from "./components/organisms/Products/Products"
+import { CategoriesDesktop } from "./components/organisms/Categories/CategoriesDesktop"
+import { LoginForm } from "./components/organisms/Login/LoginForm"
 
 function App() {
   return (
     <div>
-     <Products/>
+     {/* <CategoriesDesktop/> */}
+     {/* <Products/> */}
+     <LoginForm/>
     </div>
   );
 }
--- frontend/proyecto/src/components/molecules/CardProduct/CardProduct.jsx
+++ frontend/proyecto/src/components/molecules/CardProduct/CardProduct.jsx
@@ -13,21 +13,21 @@ export const CardProduct = ({
   location,
   category,
 }) => {
-  const [readMore, setReadMore] = useState(false);
-  const extraContent = <div>
-    <p className="extra-content">
-      {/* <Paragraph size="md" variant="secondary"> */}
-      {descripcion}
-      {/* </Paragraph> */}
-    </p>
-  </div>
-  const linkName = readMore ? '' : 'Read More';
+  // const [readMore, setReadMore] = useState(false);
+  // const extraContent = <div>
+  //   <p className="extra-content">
+  //     {/* <Paragraph size="md" variant="secondary"> */}
+  //     {descripcion}
+  //     {/* </Paragraph> */}
+  //   </p>
+  // </div>
+  // const linkName = readMore ? '' : 'Read More';
   return (
     <div className="card-product">
       <div className="card-product-img">
         <img src={url} alt={titulo} />
         <div className="fav">
-          <Icon icon="favorite" width="md" onClick={() => { }}></Icon>
+          <Icon icon="favorite" width="md" onClick={() => {}}></Icon>
         </div>
       </div>
       <div className="card-product-text">
@@ -36,29 +36,43 @@ export const CardProduct = ({
             {category.toUpperCase()}
           </Heading>
           <div className="product-cat-cat">
-            <Icon icon="star" width="xs" onClick={() => { }}></Icon>
-            <Icon icon="star" width="xs" onClick={() => { }}></Icon>
-            <Icon icon="star" width="xs" onClick={() => { }}></Icon>
-            <Icon icon="star" width="xs" onClick={() => { }}></Icon>
-            <Icon icon="star" width="xs" onClick={() => { }}></Icon>
+            <Icon icon="star" width="xs" onClick={() => {}}></Icon>
+            <Icon icon="star" width="xs" onClick={() => {}}></Icon>
+            <Icon icon="star" width="xs" onClick={() => {}}></Icon>
+            <Icon icon="star" width="xs" onClick={() => {}}></Icon>
+            <Icon icon="star" width="xs" onClick={() => {}}></Icon>
           </div>
         </div>
         <Heading type="md" title="h3" variant="primary">
           {titulo}
         </Heading>
         <div className="product-location">
-          <Icon icon="location" width="xs" onClick={() => { }}></Icon>
-          <Paragraph size="md" variant="secondary">{location} <Span size="md" variant="aqua"> MOSTRAR EN EL MAPA</Span></Paragraph>
+          <Icon icon="location" width="xs" onClick={() => {}}></Icon>
+          <Paragraph size="md" variant="secondary">
+            {location}
+            <Span size="md" variant="aqua">
+              MOSTRAR EN EL MAPA
+            </Span>
+          </Paragraph>
         </div>
-
-
         {/* <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h2>{linkName}</h2></a>
       {readMore && extraContent} */}
-      <div className="product-description">
-        <Paragraph size="md" variant="secondary">{descripcion}</Paragraph>
+        <div className="product-description">
+          <Paragraph size="md" variant="secondary">
+            {descripcion} 
+            <Span size="md" variant="aqua">
+              ver más
+            </Span>
+          </Paragraph>
 
-        <Button size="sm" label="ver detalle" variant="primary" onClick={() => { setReadMore(!readMore) }}></Button>
-      </div>
+          {/* <Button size="sm" label="ver detalle" variant="primary" onClick={() => { setReadMore(!readMore) }}></Button> */}
+          <Button
+            size="sm"
+            label="ver detalle"
+            variant="primary"
+            onClick={() => {}}
+          ></Button>
+        </div>
       </div>
     </div>
   );
