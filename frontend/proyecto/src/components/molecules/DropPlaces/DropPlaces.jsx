73003869dbdc050acddc0a6cede84eb71a7f20a1
mirrabenjamin@gmail.com <mirrabenjamin@gmail.com>
2022-05-24 17:33:14 -0300

    agregando dropplaces ft2

--- frontend/proyecto/src/components/molecules/DropPlaces/DropPlaces.jsx
+++ frontend/proyecto/src/components/molecules/DropPlaces/DropPlaces.jsx
@@ -58,18 +58,26 @@ export const DropPlaces = ({ placeholder, icon }) => {
         }
     }
 
+    const inputLugar = document.getElementById("inputLugar");
+    
+    const handlePlace = (lugar)=>{
+        setLugarFiltrado([]);
+        return inputLugar.value = lugar;
+    }
+
     return (
         <div className="search">
+           {console.log(inputLugar)}
             <div className="searchInputIcon">
                 <Icon className="icon" icon={icon.icon} width={icon.width} />
-                <input type="text" placeholder={placeholder} onChange={handleFilter}></input>
+                <input type="text" id="inputLugar" placeholder={placeholder} onChange={handleFilter}></input>
             </div>
             {lugarFiltrado.length !== 0 && (
                 <div className="searchResult">
                     {lugarFiltrado.map((lugar, key) => {
                         return (
                             <>
-                            <div key={key++} className="lugar">
+                            <div key={key++} className="lugar" onClick={()=>handlePlace(lugar.info.ciudad + ", " + lugar.info.pais)}>
                                 <div className="lugarIcon">
                                     <Icon className="" icon={lugar.info.icon} width="lg" />
                                 </div>
