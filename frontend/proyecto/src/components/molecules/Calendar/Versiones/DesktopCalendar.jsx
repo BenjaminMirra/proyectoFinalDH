29f8329e709b941d4e0f9ece0e19111001498220
mirrabenjamin@gmail.com <mirrabenjamin@gmail.com>
2022-06-01 11:06:12 -0300

    agregando calendarioNuevo

---
+++ frontend/proyecto/src/components/molecules/Calendar/Versiones/DesktopCalendar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Heading } from '../../../atoms/Heading/Heading'
+import { Button } from '../../../atoms/Button/Button'
+import { DateRangePicker } from 'rsuite';
+import { CustomProvider } from 'rsuite';
+import esAR from 'rsuite/locales/es_AR';
+import format from 'date-fns/format';
+
+import 'rsuite/dist/rsuite.min.css';
+import '../../DropCalendar/CalendarDrop.css';
+import './DesktopCalendar.css';
+import { Calendario } from '../../CalendarioNuevo/Calendario';
+
+
+export const DesktopCalendar = () => {
+
+    return (
+        <div className="desktop-calendar">
+            <div className='desktop-calendar-heading'>
+                <Heading title="h1" type="lg" variant="primary">
+                    Fechas Disponibles
+                </Heading>
+            </div>
+            <div className='desktop-calendar-reserva'>
+                <div className='desktop-calendario'>
+                    <Calendario/>
+                    {/* <CustomProvider locale={esAR}>
+                        <DateRangePicker open={true} size="md" placeholder="Check in - Check out" showOneCalendar={false} format={"dd MMMM y"} locale={{
+                            sunday: 'D',
+                            monday: 'L',
+                            tuesday: 'M',
+                            wednesday: 'X',
+                            thursday: 'J',
+                            friday: 'V',
+                            saturday: 'S',
+                            ok: 'Aplicar',
+                            today: 'Today',
+                        }} renderValue={(value) => {
+                            return format(value[0], "dd") + " de " + format(value[0], 'MMM').toLowerCase() + ". - " + format(value[1], "dd") + " de " + format(value[1], 'MMM').toLowerCase() + "."
+                        }}
+                        />
+                    </CustomProvider> */}
+                </div>
+                <div className='desktop-reserva'>
+                    <div className='desktop-reserva-heading'>
+                        <Heading title="h4" type="sm" variant="primary">
+                            Agregá tus fechas de viaje para obtener precios exactos
+                        </Heading>
+                    </div>
+                    <div className='desktop-reserva-button'>
+                        <Button size='base' label='Iniciar Reserva'></Button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
