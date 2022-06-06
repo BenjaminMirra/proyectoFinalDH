a57774f7b19d521bcb2221781bf810a54f5be916
mirrabenjamin@gmail.com <mirrabenjamin@gmail.com>
2022-06-06 17:03:13 -0300

    Filter para mobile y tablet

--- frontend/proyecto/src/components/organisms/Filter/Versions/DesktopFilter.jsx
+++ frontend/proyecto/src/components/organisms/Filter/Versions/DesktopFilter.jsx
@@ -1,58 +1,68 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './DesktopFilter.css'
 import { DropPlaces } from '../../../molecules/DropPlaces/DropPlaces'
 import { Heading } from '../../../atoms/Heading/Heading'
 import { Button } from '../../../atoms/Button/Button'
-import { SpacerHorizontal } from '../../../atoms/Spacer/SpacerHorizontal' 
+import { SpacerHorizontal } from '../../../atoms/Spacer/SpacerHorizontal'
 import { CalendarDrop } from '../../../molecules/DropCalendar/CalendarDrop'
 import axios from 'axios'
 export const DesktopFilter = (props) => {
-  
-  const handleSubmit=(e)=>{
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    if (props) {  
+    if (props) {
       console.log(props.lugarInput);
-      let ciudadId=undefined;
-      if (props.lugarInput=='San Carlos de Bariloche') {
-        ciudadId=1
+      let ciudadId = undefined;
+      if (props.lugarInput == 'San Carlos de Bariloche') {
+        ciudadId = 1
         props.setLugarInput('')
-        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data=>props.setFilterProducts(data.data))
+        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
       }
-      else if(props.lugarInput=='Buenos Aires'){
-        ciudadId=2;
+      else if (props.lugarInput == 'Buenos Aires') {
+        ciudadId = 2;
         props.setLugarInput('')
-        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data=>props.setFilterProducts(data.data))
+        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
       }
-      else{
+      else if (props.lugarInput == 'Mendoza') {
+        ciudadId = 3;
+        props.setLugarInput('')
+        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+      }
+      else if (props.lugarInput == 'Córdoba') {
+        ciudadId = 4;
+        props.setLugarInput('')
+        axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+      }
+      else {
         console.log('entro');
         props.setLugarInput('')
         props.setFilterProducts([])
       }
-        
+
     }
   }
-  
-  
-  
-  
-  
+
+
+
+
+
   return (
-     <div className='desktop-filter-container'>
-        
-            <div className='desktop-filter-container-content'>
-            <Heading variant='base' type='xl'  >Busca ofertas en hoteles, casas y mucho más</Heading>
-            <SpacerHorizontal height='sm' />
-            <form onSubmit={handleSubmit}>
-            <div className='desktop-filters-button'>
-                <DropPlaces lugarInput={props.lugarInput} setLugarInput={props.setLugarInput} placeholder='¿A donde vamos?' icon='location'/>
-                <div style={{width:'40%'}} >
-                <CalendarDrop/>
-                </div>
-                <Button type='submit' size='base' label='Buscar'></Button>
+    <div className='desktop-filter-container'>
+
+      <div className='desktop-filter-container-content'>
+        <Heading variant='base' type='xl'  >Busca ofertas en hoteles, casas y mucho más</Heading>
+        <SpacerHorizontal height='sm' />
+        <form onSubmit={handleSubmit}>
+          <div className='desktop-filters-button'>
+            <DropPlaces lugarInput={props.lugarInput} setLugarInput={props.setLugarInput} placeholder='¿A donde vamos?' icon='location' />
+            <div style={{ width: '40%' }} >
+              <CalendarDrop />
             </div>
-            </form>
-            </div>
-        </div>
+            <Button type='submit' size='base' label='Buscar'></Button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- frontend/proyecto/src/components/organisms/Filter/Versions/MobileFilter.jsx
+++ frontend/proyecto/src/components/organisms/Filter/Versions/MobileFilter.jsx
@@ -1,28 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { DropPlaces } from '../../../molecules/DropPlaces/DropPlaces'
-
+import axios from 'axios'
 import './MobileFilter.css'
 import { Heading } from '../../../atoms/Heading/Heading'
 import { Button } from '../../../atoms/Button/Button'
 import { SpacerHorizontal } from '../../../atoms/Spacer/SpacerHorizontal'
 import { CalendarDrop } from '../../../molecules/DropCalendar/CalendarDrop'
 
-export const MobileFilter = ({}) => {
+export const MobileFilter = ( props ) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (props) {
+            console.log(props.lugarInput);
+            let ciudadId = undefined;
+            if (props.lugarInput == 'San Carlos de Bariloche') {
+                ciudadId = 1
+                props.setLugarInput('')
+                axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+            }
+            else if (props.lugarInput == 'Buenos Aires') {
+                ciudadId = 2;
+                props.setLugarInput('')
+                axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+            }
+            else if (props.lugarInput == 'Mendoza') {
+                ciudadId = 3;
+                props.setLugarInput('')
+                axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+            }
+            else if (props.lugarInput == 'Córdoba') {
+                ciudadId = 4;
+                props.setLugarInput('')
+                axios.get(`http://localhost:8080/productos/porCiudad/${ciudadId}`).then(data => props.setFilterProducts(data.data))
+            }
+            else {
+                console.log('entro');
+                props.setLugarInput('')
+                props.setFilterProducts([])
+            }
+
+        }
+    }
+
+
     return (
         <div className='mobile-filter-container'>
             <div className='mobile-filter-container-content'>
-            <Heading variant='base' type='xl'  >Busca ofertas en hoteles, <br /> casas y mucho más</Heading>
-            <SpacerHorizontal height='sm' />
-            
-            <DropPlaces placeholder='¿A donde vamos?' icon='location'/>
-            <SpacerHorizontal height='xxs' />
-            <div style={{width:'100%'}}>
-            <CalendarDrop/>
-            </div>
-            <SpacerHorizontal height='xxs' />
-            <Button size='xl' label='Buscar'></Button>
-            
+                <Heading variant='base' type='xl'  >Busca ofertas en hoteles, <br /> casas y mucho más</Heading>
+                <SpacerHorizontal height='sm' />
+                <form onSubmit={handleSubmit}>
+                    <div className="mobile-filters-button">
+                        <DropPlaces lugarInput={props.lugarInput} setLugarInput={props.setLugarInput} placeholder='¿A donde vamos?' icon='location' />
+                        <SpacerHorizontal height='xxs' />
+                        <div style={{ width: '100%' }}>
+                            <CalendarDrop />
+                        </div>
+                        <SpacerHorizontal height='xxs' />
+                        <Button type='submit' size='xl' label='Buscar'></Button>
+                    </div>
+                </form>
             </div>
         </div>
     )
