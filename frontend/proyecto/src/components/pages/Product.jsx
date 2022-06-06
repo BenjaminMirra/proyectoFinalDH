766ee79021415e041c558b1b1aa827f54bc8cc54
mirrabenjamin@gmail.com <mirrabenjamin@gmail.com>
2022-06-06 17:36:51 -0300

    agregando location (ciudad,provincia,país) en product-page

--- frontend/proyecto/src/components/pages/Product.jsx
+++ frontend/proyecto/src/components/pages/Product.jsx
@@ -1,5 +1,5 @@
-import React,{useState,useEffect,createContext} from 'react'
-import {useParams} from "react-router-dom";
+import React, { useState, useEffect, createContext } from 'react'
+import { useParams } from "react-router-dom";
 import { Header } from '../organisms/Header/Header'
 import { ProductHeader } from '../molecules/ProductHeader/ProductHeader'
 import { LocationInfo } from '../molecules/LocationInfo/LocationInfo'
@@ -14,47 +14,52 @@ import axios from 'axios';
 import './Product.css'
 
 export const Product = () => {
-  
-  
 
-  const {id}=useParams()
-  const [productData,setProductData]=useState({})
-  const [categoria,setCategoria]=useState('cargando')
+
+
+  const { id } = useParams()
+  const [productData, setProductData] = useState({})
+  const [categoria, setCategoria] = useState('cargando')
   // const images=["https://images.unsplash.com/photo-1629794226066-349748040fb7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770", "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740", "https://images.unsplash.com/photo-1609766857041-ed402ea8069a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740",  "https://images.unsplash.com/photo-1623625434462-e5e42318ae49?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771", "https://images.unsplash.com/photo-1629794226066-349748040fb7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770"];
   useEffect(() => {
     window.scrollTo(0, 0);
-    axios.get(`http://localhost:8080/productos/${id}`).then(data=>
-    {
-      setCategoria(data.data.categoria_id==1?'Hoteles':data.data.categoria_id==2?'Hosteles':data.data.categoria_id==3?'Departamentos':'Bed & Breakfast')
-      setProductData(data.data)})
+    axios.get(`http://localhost:8080/productos/${id}`).then(data => {
+      setCategoria(data.data.categoria_id == 1 ? 'Hoteles' : data.data.categoria_id == 2 ? 'Hosteles' : data.data.categoria_id == 3 ? 'Departamentos' : 'Bed & Breakfast')
+      setProductData(data.data)
+    })
   }, [id]);
 
-  
+  const [ciudadData, setCiudadData] = useState({})
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/ciudades/todas`).then(data => {
+      setProductData(data.data)
+    })
+  }, []);
+
+
   return (
     <div className='wrapper'>
       <Header />
-      
-      
-      
       <div className='contenedor-pagina' >
-        
-          <ProductHeader category={categoria} title={productData.titulo} />
-          <LocationInfo location={productData.direccion} rating={productData.puntaje} />
-          <Carousel product={productData} />
-          <ProductDescription subtitle={productData.titulo_descripcion} description={productData.descripcion} />
-          <ProductServices services={productData.caracteristicasDTOList} />
-          <Calendar />
-          <Map lat={productData.latitud} lng={productData.longitud} location={productData.direccion} />
-          <ProductInfo info={productData.politicaListDTO} />
-          
-        
-        
-        
-       
+
+        <ProductHeader category={categoria} title={productData.titulo} />
+        <LocationInfo location={productData.ciudad_id==1?'San Carlos de Bariloche, Río Negro, Argentina':productData.ciudad_id==2?'Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina':productData.ciudad_id==3?'Mendoza, Mendoza, Argentina':'Córdoba, Córdoba, Argentina'} rating={productData.puntaje} />
+        <Carousel product={productData} />
+        <ProductDescription subtitle={productData.titulo_descripcion} description={productData.descripcion} />
+        <ProductServices services={productData.caracteristicasDTOList} />
+        <Calendar />
+        <Map lat={productData.latitud} lng={productData.longitud} location={productData.direccion} />
+        <ProductInfo info={productData.politicaListDTO} />
+
+
+
+
+
         <div className="product-footer">
           <Footer />
         </div>
-        
+
       </div>
 
     </div>
