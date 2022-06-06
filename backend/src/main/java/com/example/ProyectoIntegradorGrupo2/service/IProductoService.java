1035008eba55f943a8fad37efabda262527d831d
Flor243 <florencia.fernandez.051@gmail.com>
2022-06-06 10:41:47 -0300

    agregando metodo de obtener todos ordenados

--- backend/src/main/java/com/example/ProyectoIntegradorGrupo2/service/IProductoService.java
+++ backend/src/main/java/com/example/ProyectoIntegradorGrupo2/service/IProductoService.java
@@ -11,6 +11,7 @@ public interface IProductoService {
     ProductoDTO agregarProducto(ProductoDTO productoDTO) throws BadRequestException;
     ProductoDTO obtenerProductoPorId(Long id) throws ResourceNotFoundException;
     List<ProductoDTO> listarTodos();
+    List<ProductoDTO> listarTodosOrdenados();
     ProductoDTO editar(ProductoDTO productoDTO)throws ResourceNotFoundException,BadRequestException;
     void eliminar(Long id)throws ResourceNotFoundException;
     List<ProductoDTO> buscarProductosPorCategoria(Long id) throws ResourceNotFoundException;
