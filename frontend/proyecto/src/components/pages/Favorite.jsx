47ba5e3714a6fa853798e27de56e7338e3f5f977
mirrabenjamin@gmail.com <mirrabenjamin@gmail.com>
2022-06-24 12:25:16 -0300

    agregando mis reservas

--- frontend/proyecto/src/components/pages/Favorite.jsx
+++ frontend/proyecto/src/components/pages/Favorite.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Header } from "../organisms/Header/Header";
 import { Footer } from "../organisms/Footer/Footer";
-import { Picture } from "../atoms/Picture/Picture";
 import { Heading } from "../atoms/Heading/Heading";
-import { Paragraph } from "../atoms/paragraph/Paragraph";
-import { Button } from "../atoms/Button/Button";
-import { MockUp } from "../molecules/MockUpCard/MockUp";
 import { Products } from "../organisms/Products/Products";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { urlAPI } from "../../global";
 import { Mapa } from "./Mapa";
@@ -16,75 +12,75 @@ import { Icon } from "../atoms/Icon/Icon";
 
 export const Favorite = () => {
 
-  
+
 
   const [favoriteProducts, setFavoriteProducts] = useState('');
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [favoriteLocations, setFavoriteLocations] = useState([]);
   useEffect(() => {
     setFavoriteProducts('');
-    const userId=JSON.parse(localStorage.getItem('userData')).id;
+    const userId = JSON.parse(localStorage.getItem('userData')).id;
     axios.get(`${urlAPI}reacciones/porUsuario/${userId}`).then((data) => {
-      
+
       setFavoriteProducts('');
-      
+
       data.data.forEach((element) => {
         // console.log(element);
 
         axios.get(`${urlAPI}productos/${element.producto_id}`).then((data) => {
           // console.log(data.data.id);
-               setFavoriteLocations((prevData) => {
-                 return [
-                   ...prevData,
-                   {
-                     id: data.data.id,
-                     lat: data.data.latitud,
-                     lng: data.data.longitud,
-                   },
-                 ];
-               });
-                setFavoriteProducts(prevData=>{
-                    // console.log(prevData);
-                    // if (prevData.length()>0) {
-                    //      prevData.forEach((element) => {
-                    //        if (element.id == data.data.id) {
-                    //          return prevData;
-                    //        } else {
-                    //          return [
-                    //            ...prevData,
-                    //            { id: data.data.id, product: data.data },
-                    //          ];
-                    //        }
-                    //      });
-                    // }
-                     
-                     if(!prevData){
-                       
-                        
-                        return [{ id: data.data.id, product: data.data },
-                        ];
-                     }
-                     else{
-                        let bandera=false
-                        prevData.forEach(element => {
-                            if (element.id == data.data.id) {
-                                return bandera=true;
-                            }
-                            
-                        });
-                        if(!bandera){
-                            
-                            return [
-                                ...prevData,
-                                { id: data.data.id, product: data.data },
-                            ];
-                        }
-                        else{
-                            return prevData;
-                        }
-                     }
-                })
-          
+          setFavoriteLocations((prevData) => {
+            return [
+              ...prevData,
+              {
+                id: data.data.id,
+                lat: data.data.latitud,
+                lng: data.data.longitud,
+              },
+            ];
+          });
+          setFavoriteProducts(prevData => {
+            // console.log(prevData);
+            // if (prevData.length()>0) {
+            //      prevData.forEach((element) => {
+            //        if (element.id == data.data.id) {
+            //          return prevData;
+            //        } else {
+            //          return [
+            //            ...prevData,
+            //            { id: data.data.id, product: data.data },
+            //          ];
+            //        }
+            //      });
+            // }
+
+            if (!prevData) {
+
+
+              return [{ id: data.data.id, product: data.data },
+              ];
+            }
+            else {
+              let bandera = false
+              prevData.forEach(element => {
+                if (element.id === data.data.id) {
+                  return bandera = true;
+                }
+
+              });
+              if (!bandera) {
+
+                return [
+                  ...prevData,
+                  { id: data.data.id, product: data.data },
+                ];
+              }
+              else {
+                return prevData;
+              }
+            }
+          })
+
         });
 
         //
@@ -94,37 +90,35 @@ export const Favorite = () => {
   useEffect(() => {
     setSelectedProducts([]);
     if (favoriteProducts) {
-        favoriteProducts.forEach((element) => {
-            return setSelectedProducts(prevData=>[...prevData,element.product])
-        });
-         
+      favoriteProducts.forEach((element) => {
+        return setSelectedProducts(prevData => [...prevData, element.product])
+      });
     }
-   
   }, [favoriteProducts]);
 
-  const navigate=useNavigate();
-  const handleBack=()=>{
+  const navigate = useNavigate();
+  const handleBack = () => {
     navigate(-1)
   }
-    
-const [showMap, setShowMap] = useState(true);
-const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-useEffect(() => {
-  function handleResize() {
-    setWindowWidth(window.innerWidth);
-  }
-  window.addEventListener("resize", handleResize);
-  return () => window.removeEventListener("resize", handleResize);
-}, [windowWidth]);
+
+  const [showMap, setShowMap] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowWidth]);
   const [randomValue, setRandomValue] = useState(0);
-useEffect(() => {
-  if (windowWidth < 1120) {
-    setShowMap(false);
-  } else {
-    setShowMap(true);
-    setRandomValue(prevData=>prevData+1);
-  }
-}, [windowWidth]);
+  useEffect(() => {
+    if (windowWidth < 1120) {
+      setShowMap(false);
+    } else {
+      setShowMap(true);
+      setRandomValue(prevData => prevData + 1);
+    }
+  }, [windowWidth]);
 
 
 
